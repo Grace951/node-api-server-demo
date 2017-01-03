7a44ec4b410e4c0548bb60d378ee21ac631c13e9
Grace951 <learningg@gmail.com>
2017-01-03 12:39:41 +0800

    initial version

---
+++ api_server.js
@@ -0,0 +1,15 @@
+/* eslint no-console: 0 */
+var express = require('express');
+var path = require('path');
+
+var apiPort = process.env.PORT || 3000;
+var app = express();
+app.use( express.static(path.resolve(__dirname, './public/json')));
+
+app.listen(apiPort, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.info(`Api listening on port ${apiPort}!`);
+  }
+});

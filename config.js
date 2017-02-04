dadb23c2a19a724abf96f55aa7e93ff01b63068b
Grace951 <learningg@gmail.com>
2017-02-04 14:00:50 +0800

    config

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ module.exports = {
 			callbackURL: "http://127.0.0.1:3003/auth/github/callback",
 		},
 		google: {
-			APP_ID: '586155954929-m97mht8fe5sm5ua26pjbu3bkij22p8i0.apps.googleusercontent.com',
-			APP_SECRET: 'bAW5aAv1rj1NVA4-2eW2eeoB',
+			APP_ID: 'get_your_own',
+			APP_SECRET: 'get_your_own',
 			callbackURL: "http://localhost:3003/auth/google/callback",
 		},
 		facebook: {
-			APP_ID: '603952179792001',
-			APP_SECRET: '5938865a2617c7d0ac520880fc23062b',
+			APP_ID: 'get_your_own',
+			APP_SECRET: 'get_your_own',
 			callbackURL: "http://localhost:3003/auth/facebook/callback",
 		}
 	}
